a62b4fa6e8e8e64bae6701566c3cc978d659c418
Teo <onlyteo@live.com>
2016-10-15 18:56:11 +0200

    Added scripts for applying and removing IP filers on UFW

---
+++ cdir/ufw-apply-deny-ip-range.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT="${BASH_SOURCE-$0}"
+SCRIPT_DIR="$( dirname "${SCRIPT}" )"
+
+if [ $# < 1 ]; then
+   echo "CDIR filename must be passed as an argument"
+   echo "${SCRIPT} <cdir-file>"
+   exit 1
+fi
+
+CDIR_FILE="$1"
+
+if [ ! -f ${CDIR_FILE} ]; then
+   echo "Cannot find CDIR file $1 in folder ${SCRIPT_DIR}"
+   exit 1
+fi
+
+while IFS='' read -r line || [[ -n "${line}" ]]; do
+   if [[ ! "${line}" =~ ^\s*# ]]; then
+      ufw insert 1 deny from ${line} to any
+   else
+      echo "Skipping commented line"
+   fi
+done < "${CDIR_FILE}"
